f09af6f1d847dc10a37529c2353bd736e868e48d
akashvadnala <akashvadnala123@gmail.com>
2023-02-03 23:44:34 +0530

    Login

--- client/src/Routing.js
+++ client/src/Routing.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 import Error from '../src/components/Error';
 
 const Routing = () => {
@@ -9,7 +10,8 @@ const Routing = () => {
         {/* Home */}
         <Route exact path='/' element={<Home />} />
         
-       
+       {/* Login */}
+       <Route exact path='/login' element={<Login /> } />
 
         {/* Others */}
         <Route path='*' element={<Error />} />
